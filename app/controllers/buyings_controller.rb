904c2318ad09b1d3194839bafc1a7c281d3c6986
Quynh Anh Pham <qap.pham@gmail.com>
2021-11-23 16:45:31 +0100

    buyings routes done and begun controller methods

--- app/controllers/buyings_controller.rb
+++ app/controllers/buyings_controller.rb
@@ -1,2 +1,12 @@
 class BuyingsController < ApplicationController
+
+  def new
+    @material = Material.find(params[:material_id])
+    @buying = Buying.new
+  end
+
+  def create
+    @buying = Buying.find
+
+  end
 end
