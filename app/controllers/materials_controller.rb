eb9a34fb5cd6d6f97595ce66f1c8a0ff4f4a3e95
Quynh Anh Pham <qap.pham@gmail.com>
2021-11-23 13:05:18 +0100

    New and create for the material done

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,25 +1,29 @@
 class MaterialsController < ApplicationController
-    def index
-      @materials = Material.all
-    end
+  def index
+    @materials = Material.all
+  end
 
-    def new
-      @material = Material.new
-    end
+  def show
+    @material = Material.find(params[:id])
+  end
 
-    def create
-      @material = Material.new(material_params)
-      @material.user_id = current_user.id
-      if @material.save
-        redirect_to material_path(@material)
-      else
-        render :new
-      end
-    end
+  def new
+    @material = Material.new
+  end
 
-    private
-
-    def material_params
-      params.require(:material).permit(:name, :description, :location, :price, photos: [])
+  def create
+    @material = Material.new(material_params)
+    @material.user_id = current_user.id
+    if @material.save
+      redirect_to material_path(@material)
+    else
+      render :new
     end
+  end
+
+  private
+
+  def material_params
+    params.require(:material).permit(:name, :description, :location, :price, photos: [])
+  end
 end
