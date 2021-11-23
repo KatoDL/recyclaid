97f766cecfb700b15e3f4b7352008cefbac37524
Dorian della Faille <dorian.della.faille@gmail.com>
2021-11-23 15:51:34 +0100

    advancing on the showpage of the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
 
   def show
-  @user = User.find(params[:id])
-  @materials = @user.materials
-  # @reviews = []
-  # @materials.each do |material|
-  #   if material.review
-  #     @reviews << material.review
-  #   end
-  # end
+    @user = User.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ Material.destroy_all
 User.destroy_all
 
 puts "Creating materials..."
-user1 = User.create!({ email: "Quynh-Anh@lewagon.be", password: "recyclaid"})
-user2 = User.create!({ email: "kato@lewagon.be", password: "recylaid" })
-user3 = User.create!({ email: "dorian@lewagon.be", password: "recyclaid"})
+user1 = User.create!({ email: "Quynh-Anh@lewagon.be", password: "recyclaid", name: "Quynh-Anh" description: "My windows are the best, don't even consider buying from someone else." })
+user2 = User.create!({ email: "kato@lewagon.be", password: "recylaid", name: "Kato" description: "The wood I provide is always fresh from the day, I chop it myself :)" })
+user3 = User.create!({ email: "dorian@lewagon.be", password: "recyclaid", name: "Dorian", description: "I am a true sand lover !" })
 
 material1 = Material.create!({ name: "Kato's wood", description: "I have some wood left over from breaking down my shed.", location: "Cantersteen 12, 1000 Bruxelles", price: 5, user: user2 })
 material2 = Material.create!({ name: "Quynh-Anh's windows", description: "I bought the wrong windows for my house, so now I'm selling them here.", location: "Rue du MArché aux Herbes 56, 1000 Bruxelles", price: 4, user: user1 })
