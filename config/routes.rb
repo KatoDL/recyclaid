424f212fb4e899a2e8f40a867e843a0be1cdc98c
Quynh Anh Pham <qap.pham@gmail.com>
2021-11-23 17:54:14 +0100

    Fixing conflict in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   resources :materials do
     resources :buyings, only: [:new, :create]
   end
-=======
-  resources :materials
   resources :users, only: [:show]
->>>>>>> master
 end
