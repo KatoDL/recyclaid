b4aaaf1f3011fca1f5bc9e3a9bae4b73b8170569
Dorian della Faille <dorian.della.faille@gmail.com>
2021-11-23 12:19:51 +0100

    merging with new master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+
+  def show
+  @user = User.find(params[:id])
+  @materials = @user.materials
+  # @reviews = []
+  # @materials.each do |material|
+  #   if material.review
+  #     @reviews << material.review
+  #   end
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,4 +16,4 @@ user3 = User.create!({ email: "dorian@lewagon.be", password: "recyclaid"})
 
 material1 = Material.create!({ name: "Kato's wood", description: "I have some wood left over from breaking down my shed.", location: "Cantersteen 12, 1000 Bruxelles", price: 5, user: user2 })
 material2 = Material.create!({ name: "Quynh-Anh's windows", description: "I bought the wrong windows for my house, so now I'm selling them here.", location: "Rue du MArché aux Herbes 56, 1000 Bruxelles", price: 4, user: user1 })
-material3 = Material.create!({ name: "Dorian's sand", description: "I am building my house myself and I dug up a lot of sand!", location: "Chaussée d'Alsemberg 252, 1190 Forest", price: 4, user: user3 })
+material3 = Material.create!({ name: "Dorian's sand", description: "I am building my house myself and I dug up a lot of sand!", location: "Chaussée d'Alsemberg 252, 1190 Forest", price: 65, user: user3 })
