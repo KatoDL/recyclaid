6de751fbda57b02290b55cfc0afe475d44737da2
Dorian della Faille <dorian.della.faille@gmail.com>
2021-11-24 15:48:19 +0100

    own items added to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,3 +8,17 @@ class PagesController < ApplicationController
     @user = current_user
   end
 end
+
+
+  # def dashboard
+  #   @user = current_user
+  #   @bookings = @user.bookings
+
+  #   @toilets = Toilet.where(user: @user)
+  #   @bookings_of_your_toilets = []
+  #   @toilets.each do |toilet|
+  #     toilet.bookings.each do |booking|
+  #       @bookings_of_your_toilets << booking
+  #     end
+  #   end
+  # end
