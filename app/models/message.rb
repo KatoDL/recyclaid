7e455de1876194166987ea9b74a1693b2c94e5d1
Quynh Anh Pham <qap.pham@gmail.com>
2021-11-25 17:50:15 +0100

    Parent_id in the message model commented out

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :buying
-  belongs_to :parent, class_name: 'Message', optional: true
-  has_many :replies, class_name: 'Message', foreign_key: :parent_id, dependent: :destroy
+  # belongs_to :parent, class_name: 'Message', optional: true
+  # has_many :replies, class_name: 'Message', foreign_key: :parent_id, dependent: :destroy
 
   # validates :content, presence: true
 end
