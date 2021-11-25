e3f3388aade39aeb6a93e9eb91bef8f158fab8d1
Quynh Anh Pham <qap.pham@gmail.com>
2021-11-25 11:56:52 +0100

    Show of a buying done

--- app/controllers/buyings_controller.rb
+++ app/controllers/buyings_controller.rb
@@ -1,20 +1,30 @@
 class BuyingsController < ApplicationController
+  before_action :set_material, only: [:show, :new, :create]
+
+  def show
+    @buying = Buying.find(params[:id])
+  end
 
   def new
-    @material = Material.find(params[:material_id])
     @buying = Buying.new
   end
 
   def create
-    @material = Material.find(params[:material_id])
     @buying = Buying.new(buying_params)
     @buying.material = @material
     @buying.user = current_user
-    @buying.save!
-    redirect_to material_path(@material)
+    if @buying.save
+      redirect_to material_buying_path(@material, @buying)
+    else
+      redirect_to material_path(@material)
+    end
   end
 
   private
+
+  def set_material
+    @material = Material.find(params[:material_id])
+  end
 
   def buying_params
     params.require(:buying).permit(:message, :status)
